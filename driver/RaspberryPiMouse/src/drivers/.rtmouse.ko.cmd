30033d65d2ecc0a6b82b03a18d794c9190dd19ff
asakawafuko <58972059+asakawafuko@users.noreply.github.com>
2020-01-24 13:46:13 +0900

    Delete .rtmouse.ko.cmd

--- driver/RaspberryPiMouse/src/drivers/.rtmouse.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ubuntu/RaspberryPiMouse/src/drivers/rtmouse.ko := ld -EL -r  -maarch64linux -T ./scripts/module-common.lds -T ./arch/arm64/kernel/module.lds --build-id  -o /home/ubuntu/RaspberryPiMouse/src/drivers/rtmouse.ko /home/ubuntu/RaspberryPiMouse/src/drivers/rtmouse.o /home/ubuntu/RaspberryPiMouse/src/drivers/rtmouse.mod.o ;  true
